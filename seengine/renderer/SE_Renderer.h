b75ec64391717c333a5140977a7d5037e44c1a47
huhuhu1092 <wei.lu95@gmail.com>
2010-04-23 15:27:59 +0800

    change SE_Renderer

--- seengine/renderer/SE_Renderer.h
+++ seengine/renderer/SE_Renderer.h
@@ -13,7 +13,11 @@ typedef struct SE_Renderer_tag
 {
 
 } SE_Renderer ;   
-
+extern SE_Result SE_Renderer_Init(SE_Renderer* renderer);
+extern SE_Result SE_Renderer_BeginDraw(SE_Renderer* renderer);
+extern SE_Result SE_Renderer_EndDraw(SE_Renderer* renderer);
+extern SE_Result SE_Renderer_Draw(SE_Renderer* renderer);
+extern void SE_Renderer_Release(SE_Renderer* renderer);
 #ifdef __cplusplus
 }
 #endif /** end __cplusplus*/
