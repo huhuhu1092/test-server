a8d9248c4128ab6b18b029aa38771f8fdd3252a1
luwei <luwei@luwei-desktop.(none)>
2010-07-05 01:00:43 +0800

    add c++ code

---
+++ seengine_c++/SE_Common.h
@@ -0,0 +1,18 @@
+#ifndef SE_COMMON_H
+#define SE_COMMON_H
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#define SE_ASSERT(x) assert((x))
+typedef int SE_Result;
+#define SE_VALID 1
+#define SE_INVALID 0
+
+/**
+ * 0 : invalid
+ * 1 : valid
+ * */
+#define SE_Result_IsValid(r) ((r) != 0)
+enum SE_AXIS_TYPE {SE_AXIS_NOAXIS = -1, SE_AXIS_X, SE_AXIS_Y, SE_AXIS_Z};
+#endif
---
+++ seengine_c++/main.cpp
@@ -0,0 +1,6 @@
+#include "SE_Vector.h"
+#include "SE_Matrix.h"
+int main(int argc, char** argv)
+{
+    return 0;
+}
