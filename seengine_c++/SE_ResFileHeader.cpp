3e40f32aec8029ab9f6c9076c015d458c51cb182
unknown <test@.(none)>
2010-08-04 13:19:00 +0800

    cc

--- seengine_c++/SE_ResFileHeader.cpp
+++ seengine_c++/SE_ResFileHeader.cpp
@@ -1,12 +1,12 @@
 #include "SE_ResFileHeader.h"
 const int SE_MAX_MESH_NUM = 1024; //the max mesh in per scene
-const int SE_GEOMETRYDATA_ID = 0x4001;
-const int SE_TEXUNITDATA_ID = 0x4002;
-const int SE_MATERIALDATA_ID = 0x4003;
-const int SE_IMAGEDATA_ID = 0x4004;
-const int SE_SCENEDATA_ID = 0x4005;
-const int SE_MESHDATA_ID = 0x4006;
-const int SE_SHADERPROGRAMDATA_ID = 0x4007;
+const short SE_GEOMETRYDATA_ID = 0x4001;
+const short SE_TEXUNITDATA_ID = 0x4002;
+const short SE_MATERIALDATA_ID = 0x4003;
+const short SE_IMAGEDATA_ID = 0x4004;
+const short SE_SCENEDATA_ID = 0x4005;
+const short SE_MESHDATA_ID = 0x4006;
+const short SE_SHADERPROGRAMDATA_ID = 0x4007;
 const int SE_MAGIC = 0xCFCFCFCF;
 const int SE_VERSION = 0x01;
 
