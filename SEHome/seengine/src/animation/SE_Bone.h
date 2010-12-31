f4a08052707c1a2d53a42bd1ec330a77ae147534
unknown <test@.(none)>
2010-12-31 18:29:00 +0800

    add data structure about bone animation

--- SEHome/seengine/src/animation/SE_Bone.h
+++ SEHome/seengine/src/animation/SE_Bone.h
@@ -1,9 +1,57 @@
 #ifndef SE_BONE_H
 #define SE_BONE_H
+#include "SE_Common.h"
+#include "SE_Matrix.h"
+#include "SE_Vector.h"
+#include "SE_Quat.h"
 #include "SE_Matrix.h"
 #include <string>
 #include <list>
 #include <vector>
+struct SE_JointPose
+{
+	SE_Quat rotation;
+	SE_Vector3f translate;
+	SE_Vector3f scale;
+};
+struct SE_JointSample
+{
+	std::vector<SE_JointPose> jointposes;
+	std::vector<float> times;
+};
+struct SE_Joint
+{
+	SE_Matrix4f bindpose;
+	std::vector<SE_JointSample> samples;
+	int parentIndex;
+	SE_Joint()
+	{
+		parentIndex = -1;
+	}
+};
+class SE_Skeleton
+{
+public:
+	SE_Skeleton();
+	~SE_Skeleton();
+	void setJointNum(int num);
+	int getJointNum() const
+	{
+		return mJoints.size();
+	}
+    void setJoint(int index, const SE_Joint& joint)
+	{
+		SE_ASSERT(index >= 0 && index < mJoints.size());
+		mJoints[index] = joint;
+	}
+	SE_Joint getJoint(int index)
+	{
+		SE_ASSERT(index >= 0 && index < mJoints.size());
+		return mJoints[index];
+	}
+private:
+	std::vector<SE_Joint> mJoints;
+};
 class SE_Bone
 {
 public:
