6012c4de7d5028b24cc369588fc4b58729fa7261
unknown <test@.(none)>
2011-06-30 18:47:14 +0800

    add shader for alpha change

--- android/PhotoView/src/com/speedsun/PhotoView/PhotoGLRenderer.java
+++ android/PhotoView/src/com/speedsun/PhotoView/PhotoGLRenderer.java
@@ -18,18 +18,136 @@ import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.os.SystemClock;
 import android.util.Log;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.OutOfMemoryError;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 public class PhotoGLRenderer implements GLSurfaceView.Renderer {
+	private String mPicturePathName = "/sdcard/thumb";
+	private String[] mFileList;
+	private Bitmap mPicture1;
+	private Bitmap mPicture2;
+	private int mPictureIndex;
+    private class PictureNameFilter implements FilenameFilter
+    {
+    	public boolean accept(File dir, String name) {
+    		if(name.endsWith("jpeg") || name.endsWith("jpg") || name.endsWith("png") ||
+    				name.endsWith("JPEG") || name.endsWith("JPG") || name.endsWith("PNG"))
+    			return true;
+    		else
+    			return false;
+    	}
+    }
+    private void readPictureName() {
+        File pictureDir = new File(mPicturePathName);
+        if(!pictureDir.exists())
+        	return;
+        String[] fileList = pictureDir.list(new PictureNameFilter());
+        if(fileList == null)
+        {
+        	Log.i(TAG, "## has no file in " + mPicturePathName + " ####");
+        	return;
+        }
+        Log.i(TAG, "### picture num = " + fileList.length + " ####");
+        mFileList = fileList;
+    }
+    private Bitmap loadBitmap()
+    {
+		Bitmap bmp = null;
+		if(mFileList.length > 0)
+		{
+			if(mPictureIndex >= mFileList.length)
+			{
+				mPictureIndex = 0;
+			}
+			String fn = mFileList[mPictureIndex];
+			String path = mPicturePathName + "/" + fn;
+			File f = new File(path);
+			Log.i(TAG, "current image = " + path);
+
+			if(f.exists())
+			{
+				InputStream is = null;
+				try {
+				    is = new FileInputStream(f);
+				    Log.i(TAG, "#### size : " + is.available() + " ####");
+					//BitmapFactory.Options op = new BitmapFactory.Options();
+					//op.inSampleSize = 4;
+				    bmp = BitmapFactory.decodeStream(is, null, null);
+				    if(bmp != null)
+				    {
+				    	Log.i(TAG, "## bmp width, height " + bmp.getWidth() + ", " + bmp.getHeight());
+
+				    }
+				} catch (FileNotFoundException e){
+					Log.i(TAG, "file name found: " + path);
+				} catch (IOException e) {
+					Log.i(TAG, "IO error");
+				} catch (OutOfMemoryError e){
+					Log.i(TAG, "#### out of memory ####");
+					BitmapFactory.Options op = new BitmapFactory.Options();
+					op.inSampleSize = 4;
+					bmp = BitmapFactory.decodeStream(is, null, op);
+				} finally {
+					try {
+					    if(is != null)
+				            is.close();
+					} catch (IOException e) {
+						
+					}
+				}
+				
+			}
+		}
+		mPictureIndex++;
+	    return bmp;
+    }
+    void readPicture() {
+    	mPicture1 = loadBitmap();
+    	mPicture2 = loadBitmap();
+	}
     public PhotoGLRenderer(Context context) {
         mContext = context;
         mTriangleVertices = ByteBuffer.allocateDirect(mTriangleVerticesData.length
                 * FLOAT_SIZE_BYTES).order(ByteOrder.nativeOrder()).asFloatBuffer();
         mTriangleVertices.put(mTriangleVerticesData).position(0);
+        readPictureName();
+        ByteBuffer vbb = ByteBuffer.allocateDirect(mIndexData.length*4);
+        vbb.order(ByteOrder.nativeOrder());
+        mIndexBuffer = vbb.asIntBuffer();
+        mIndexBuffer.put(mIndexData);
+        mIndexBuffer.position(0);
     }
-
+    private static boolean mFirstFrame = true;
+    private long mPrevTime;
     public void onDrawFrame(GL10 glUnused) {
         // Ignore the passed-in GL10 interface, and use the GLES20
         // class's static methods instead.
-        long tStart = SystemClock.uptimeMillis();
+    	if(mFirstFrame)
+    	{
+            long tStart = SystemClock.uptimeMillis();
+            mPrevTime = tStart;
+            mFirstFrame = false;
+    	}
+    	else
+    	{
+    		long currTime = SystemClock.uptimeMillis();
+    		long span = currTime - mPrevTime;
+    		mPrevTime = currTime;
+    		Log.i(TAG, "### time span = " + span + " ####");
+    	}
+    	if(mCurrAlpha > 1.0f)
+    	{
+    		mCurrAlpha = 0.0f;
+            readPicture();
+            loadTexture(mPicture1, mTextureID);
+            loadTexture(mPicture2, mTextureID2);
+    	}
         GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
         GLES20.glUseProgram(mProgram);
@@ -37,7 +155,15 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureID);
-
+        GLES20.glUniform1i(mTexture1Handle, 0);
+        checkGlError("glUniform1i mTexture1Handler");
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureID2);
+        GLES20.glUniform1i(mTexture2Handle, 1);
+        checkGlError("glUniform1i mTexture2Handler");
+        
+        GLES20.glUniform1f(mAlphaHandle, mCurrAlpha);
+        mCurrAlpha += 0.01;
         mTriangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
         GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false,
                 TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
@@ -51,15 +177,22 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glEnableVertexAttribArray(maTextureHandle);
         checkGlError("glEnableVertexAttribArray maTextureHandle");
 
-        long time = SystemClock.uptimeMillis() % 4000L;
-        float angle = 0.090f * ((int) time);
+        //long time = SystemClock.uptimeMillis() % 4000L;
+        //float angle = 0.090f * ((int) time);
+        float angle = 0;
         Matrix.setRotateM(mMMatrix, 0, angle, 0, 0, 1.0f);
         Matrix.multiplyMM(mMVPMatrix, 0, mVMatrix, 0, mMMatrix, 0);
         Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mMVPMatrix, 0);
 
         GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
-        checkGlError("glDrawArrays");
+        //GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+        mIndexBuffer.position(0);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6, GLES20.GL_UNSIGNED_INT, mIndexBuffer);
+        checkGlError("glDrawElements");
+        
+
+        //Log.i(TAG, "### time span = " + (tEnd - tStart) + " ####");
     }
 
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
@@ -69,7 +202,22 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
         float ratio = (float) width / height;
         Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
+    private void loadTexture(Bitmap bmp, int textureid)
+    {
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureid);
 
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
+                GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER,
+                GLES20.GL_LINEAR);
+
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
+                GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
+                GLES20.GL_REPEAT);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bmp, 0);
+    }
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         // Ignore the passed-in GL10 interface, and use the GLES20
         // class's static methods instead.
@@ -94,28 +242,36 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
             throw new RuntimeException("Could not get attrib location for uMVPMatrix");
         }
 
+        mTexture1Handle = GLES20.glGetUniformLocation(mProgram, "sTexture");
+        checkGlError("glGetUniformLocation sTexture");
+        if (mTexture1Handle == -1) {
+            throw new RuntimeException("Could not get attrib location for mTexture1Handle");
+        }
+        
+        mTexture2Handle = GLES20.glGetUniformLocation(mProgram, "sTexturee");
+        checkGlError("glGetUniformLocation sTexturee");
+        if (mTexture1Handle == -1) {
+            throw new RuntimeException("Could not get attrib location for mTexture2Handle");
+        }
+        
+        mAlphaHandle = GLES20.glGetUniformLocation(mProgram, "alpha");
+        checkGlError("glGetUniformLocation alpha");
+        if (mAlphaHandle == -1) {
+            throw new RuntimeException("Could not get attrib location for mAlphaHandle");
+        }
         /*
          * Create our texture. This has to be done each time the
          * surface is created.
          */
-
-        int[] textures = new int[1];
-        GLES20.glGenTextures(1, textures, 0);
+        readPicture();
+        int[] textures = new int[2];
+        GLES20.glGenTextures(2, textures, 0);
 
         mTextureID = textures[0];
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureID);
-
-        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
-                GLES20.GL_NEAREST);
-        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                GLES20.GL_TEXTURE_MAG_FILTER,
-                GLES20.GL_LINEAR);
-
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
-                GLES20.GL_REPEAT);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
-                GLES20.GL_REPEAT);
-
+        mTextureID2 = textures[1];
+        loadTexture(mPicture1, mTextureID);
+        loadTexture(mPicture2, mTextureID2);
+        /*
         InputStream is = mContext.getResources().openRawResource(R.raw.robot);
         Bitmap bitmap;
         try {
@@ -127,10 +283,10 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
                 // Ignore.
             }
         }
-
-        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
         bitmap.recycle();
+        */
 
+        
         Matrix.setLookAtM(mVMatrix, 0, 0, 0, -5, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
     }
 
@@ -195,10 +351,17 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
     private static final int TRIANGLE_VERTICES_DATA_UV_OFFSET = 3;
     private final float[] mTriangleVerticesData = {
             // X, Y, Z, U, V
-            -1.0f, -0.5f, 0, -0.5f, 0.0f,
-            1.0f, -0.5f, 0, 1.5f, -0.0f,
-            0.0f,  1.11803399f, 0, 0.5f,  1.61803399f };
-
+    		4.0f, -4.0f, 0, 0.0f, 0.0f,
+            -4.0f, -4.0f, 0, 1.0f, 0.0f,
+            
+            -4.0f,  4.0f, 0, 1.0f,  1.0f,
+            4.0f, 4.0f, 0, 0.0f, 1.0f
+            };
+    private final int[] mIndexData = {
+    		0, 1, 2,
+    		0, 2, 3
+    };
+    private IntBuffer   mIndexBuffer;
     private FloatBuffer mTriangleVertices;
 
     private final String mVertexShader =
@@ -215,8 +378,12 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
         "precision mediump float;\n" +
         "varying vec2 vTextureCoord;\n" +
         "uniform sampler2D sTexture;\n" +
+        "uniform sampler2D sTexturee;\n" + 
+        "uniform float alpha;\n" +
         "void main() {\n" +
-        "  gl_FragColor = texture2D(sTexture, vTextureCoord);\n" +
+        "vec4 color1 = texture2D(sTexture, vTextureCoord);\n" +
+        "vec4 color2 = texture2D(sTexturee, vTextureCoord);\n" +
+        "  gl_FragColor = color1 * alpha + (1.0 - alpha) * color2;\n" +
         "}\n";
 
     private float[] mMVPMatrix = new float[16];
@@ -226,10 +393,14 @@ public class PhotoGLRenderer implements GLSurfaceView.Renderer {
 
     private int mProgram;
     private int mTextureID;
+    private int mTextureID2;
     private int muMVPMatrixHandle;
     private int maPositionHandle;
     private int maTextureHandle;
-
+    private int mTexture1Handle;
+    private int mTexture2Handle;
+    private int mAlphaHandle;
     private Context mContext;
+    private float mCurrAlpha;
     private static String TAG = "GLES20TriangleRenderer";
 }
