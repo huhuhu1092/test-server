224dd979bcfc7bc42c25dd0a49e5239187957b97
unknown <luwei@.(none)>
2011-07-13 08:11:57 +0800

    add oil algorithm

--- android/PhotoView/src/com/speedsun/PhotoView/OilifyFilter.java
+++ android/PhotoView/src/com/speedsun/PhotoView/OilifyFilter.java
@@ -1,5 +1,6 @@
 package com.speedsun.PhotoView;
 import android.graphics.Bitmap;
+
 public class OilifyFilter 
 {
 	private Bitmap mSrcBmp;
@@ -45,6 +46,81 @@ public class OilifyFilter
     {
     	return x < x1 ? x1 : (x > x2 ? x2 : x);
     }
+    private int getGray(int argb, int index)
+    {
+    	switch(index)
+    	{
+    	case 0:
+    		return (argb >> 24) & 0xFF;
+    	case 1:
+    		return (argb >> 16) & 0xFF;
+    	case 2:
+    		return (argb >> 8) &0xFF;
+    	case 3:
+    		return argb & 0xFF;
+    	default:
+    		throw new RuntimeException("index exceed");
+    	}
+    }
+private void weighted_average_color (int    hist[],
+            int    hist_rgb[][],
+            float  exponent,
+            int[] dest,
+            int    bpp, int x, int y)
+{
+    int   i, b;
+    int   hist_max = 1;
+    int   exponent_int = 0;
+    float div = 0.000001f;
+    float[] color = new float[]{ 0.0f, 0.0f, 0.0f, 0.0f };
+
+    for (i = 0; i < HISTSIZE; i++)
+        hist_max = Math.max(hist_max, hist[i]);
+
+    if ((exponent - Math.floor (exponent)) < 0.001 && exponent <= 255.0)
+        exponent_int = (int) exponent;
+
+    for (i = 0; i < HISTSIZE; i++)
+    {
+        float ratio = (float) hist[i] / (float) hist_max;
+        float weight;
+
+        if (exponent_int != 0)
+            weight = fast_powf (ratio, exponent_int);
+        else
+            weight = pow (ratio, exponent);
+
+        if (hist[i] > 0)
+            for (b = 0; b < bpp; b++)
+                color[b] += weight * (float) hist_rgb[b][i] / (float) hist[i];
+ 
+        div += weight;
+    }
+    int intv = 0;
+    for (b = 0; b < bpp; b++)
+    {
+        int c = (int) (color[b] / div);
+
+        byte v = CLAMP0255 (c);
+        switch(b)
+        {
+        case 0:
+            intv |= (v << 24) & 0xFF000000;
+            break;
+        case 1:
+        	intv |= (v << 16) & 0x00FF0000;
+        	break;
+        case 2:
+        	intv |= (v << 8) & 0x0000FF00;
+        	break;
+        case 3:
+        	intv |= v & 0x000000FF;
+        	break;
+        default:
+        	throw new RuntimeException("bpp error");
+        }
+    }
+}
     private void oilify()
     {
     	int width = mSrcBmp.getWidth();
@@ -76,15 +152,27 @@ public class OilifyFilter
     			int mask_y1 = clamp((y - radius), y1, y2);
     			int mask_x2 = clamp((x + radius + 1), x1, x2);
     			int mask_y2 = clamp((y + radius + 1), y1, y2);
-    			int src_offset = (mask_y1 - y1) * width + mask_x1 - x1;
+    			//int src_offset = (mask_y1 - y1) * width + mask_x1 - x1;
     			for(int mask_y = mask_y1 ; mask_y < mask_y2 ; mask_y++)
     			{
-    				int    dy_squared = sqr_lut[Math.abs(mask_y - y)];
+    				int  dy_squared = sqr_lut[Math.abs(mask_y - y)];
     				for(int mask_x = mask_x1 ; mask_x < mask_x2 ; mask_x++)
     				{
+    					int dx_squared = sqr_lut[Math.abs(mask_x - x)];
+    					if((dx_squared + dy_squared) > radius_square)
+    					{
+    						continue;
+    					}
+    					int inten = mIntensityBuffer[mask_y * width + mask_x];
+    					++hist[inten];
+    					for(int b = 0 ; b < 4 ; b++)
+    					{
+    						hist_rgb[b][inten] += getGray(mSrcBmpBuffer[mask_y * width + mask_x], b);
+    					}
     					
     				}
     			}
+    			weighted_average_color (hist, hist_rgb, mExponent, mSrcBmpBuffer, 4, x, y);
     		}
     	}
     }
